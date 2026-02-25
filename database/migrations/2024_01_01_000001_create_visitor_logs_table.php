af9e51010c4f0a315b3c3e3352cd569dfabd52e2
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-02-25 08:59:44 +0430

    updated

--- database/migrations/2024_01_01_000001_create_visitor_logs_table.php
+++ database/migrations/2024_01_01_000001_create_visitor_logs_table.php
@@ -10,7 +10,6 @@ return new class extends Migration
     {
         Schema::create('visitor_logs', function (Blueprint $table) {
             $table->id();
-            $table->date('date');
             $table->string('visitor_name');
             $table->string('purpose');
             $table->time('time_in');
