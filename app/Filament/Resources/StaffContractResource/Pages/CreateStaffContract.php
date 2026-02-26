43d5ecc9d76275ce2219f775ba4707e0eb22297c
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-02-26 10:13:51 +0430

    created

---
+++ app/Filament/Resources/StaffContractResource/Pages/CreateStaffContract.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\StaffContractResource\Pages;
+
+use App\Filament\Resources\StaffContractResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateStaffContract extends CreateRecord
+{
+    protected static string $resource = StaffContractResource::class;
+}
